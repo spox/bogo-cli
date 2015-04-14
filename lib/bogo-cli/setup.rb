dc7f6c3cae3926f2df83514a78ceb1085d865005
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-14 08:42:35 -0700

    Explicitly output exception message instead of relying on #to_s coercion

--- lib/bogo-cli/setup.rb
+++ lib/bogo-cli/setup.rb
@@ -19,7 +19,7 @@ module Bogo
             if(ENV['DEBUG'])
               $stderr.puts "ERROR: #{e.class}: #{e}\n#{e.backtrace.join("\n")}"
             else
-              $stderr.puts "ERROR: #{e.class}: #{e}"
+              $stderr.puts "ERROR: #{e.class}: #{e.message}"
             end
             exit e.respond_to?(:exit_code) ? e.exit_code : -1
           end
